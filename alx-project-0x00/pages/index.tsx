c5e1209e2a1e24d2f8d2417f19e5f6720cb61cd6
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-07-19 20:59:34 +0300

    Update index.tsx

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,10 +1,10 @@
-const Home: React.FC = () => {
-  return (
-    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
-      <h1>Airbnb Application Clone system</h1>
-      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
-    </main>
-  )
-}
+import React from "react";
+import Card from "@/components/Card"; // or "../components/Card" if aliasing isn't set up
 
-export default Home;
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Card />
+    </main>
+  );
+}
